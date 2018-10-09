3d67bf2f916eb202eda473a477d2c8900b3b1fa3
Scarlett <scarlett@monitorhack.com>
2018-10-10 00:35:15 +0200

    https added
    HTTPS added on post vs API

--- uwm_agent_windows.ps1
+++ uwm_agent_windows.ps1
@@ -1,4 +1,6 @@
-﻿$id = $args[0]
+﻿# POWERSHELL AGENT FOR UWEBMONITOR.COM
+
+$id = $args[0]
 $token = $args[1]
 $uptime = [int]((get-date)-[system.management.managementdatetimeconverter]::todatetime((get-wmiobject -class win32_operatingsystem).Lastbootuptime)).TotalSeconds
 
@@ -10,19 +12,19 @@ $mem_used = 100-[math]::Round(($os.FreePhysicalMemory/$os.TotalVisibleMemorySize
 $mem_total_gb = [int]($os.TotalVisibleMemorySize/1mb)
 $mem_used_gb = $mem_total_gb-[math]::Round($os.FreePhysicalMemory/1mb,2)
 
-#$id = "Windows_10"
+#$id = "Windows 10"
 #$token = "e25419d20c5912bbf41999955c1ceee5fe5d4edd"
 $data = ""
 
 $data = $data+"{"
-$data = $data+"""internal_id"": ""$id"","
-$data = $data+ """internal_token"": ""$token"","
-$data = $data+ """internal_hostOS"": ""$os"","
-$data = $data+ """internal_agentinfo_version"": 0.01,"
-$data = $data+ """cpu_perc"": $CPU,"
-$data = $data+ """mem_total_gb"": $mem_total_gb,"
-$data = $data+ """mem_used_gb"": $mem_used_gb,"
-$data = $data+ """mem_used"": $mem_used,"
+$data = $data+"""internal_id"":""$id"","
+$data = $data+ """internal_token"":""$token"","
+$data = $data+ """internal_hostOS"":""$os"","
+$data = $data+ """internal_agentinfo_version"":0.01,"
+$data = $data+ """cpu_perc"":$CPU,"
+$data = $data+ """mem_total_gb"":$mem_total_gb,"
+$data = $data+ """mem_used_gb"":$mem_used_gb,"
+$data = $data+ """mem_used"":$mem_used,"
 
 
 $array = Get-WmiObject –class win32_volume | select Name,capacity,freespace
@@ -33,8 +35,14 @@ for ($i=0; $i -lt $array.length; $i++) {
         $unit = $unit -replace "\\", ''
         $capacidad = $array[$i].capacity
         $espacio_libre = $array[$i].freespace
-        $usado_unidad = 100-[math]::round(((($espacio_libre*100)/$capacidad)),2)
-        $data = $data+ """filesystem_$unit"": $usado_unidad,"
+#        echo $espacio_libre
+#        echo $capacidad
+        if (($espacio_libre -eq 0) -Or ($capacidad -eq 0)) {
+            $usado_unidad = 100            
+        } else {
+            $usado_unidad = 100-[math]::round(((($espacio_libre*100)/$capacidad)),2)
+        }
+        $data = $data+ """filesystem_$unit"":$usado_unidad,"
     }
 }
 
@@ -45,19 +53,19 @@ for ($i=0; $i -lt $array.length; $i++) {
     $service_state = $array[$i].Started
     $service_state_output = "false"
     if ($service_state) { $service_state_output = "true" }   
-    $data = $data+ """service_$service_name"": $service_state_output,"
+    $data = $data+ """service_$service_name"":$service_state_output,"
 }
 
 
 $data = $data+ """uptime"": $uptime"
 $data = $data+ "}"
+#$data = get-content -encoding utf8 $data 
+echo $data
 
-#$JSON = $data | convertto-json
-$JSON = $data
-echo $JSON
 
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $uri = "https://www.uwebmonitor.com/site/api/api.php"
-
-$api = Invoke-WebRequest -UseBasicParsing -uri $uri -Method POST -Body $JSON
+$api = Invoke-WebRequest -UseBasicParsing -uri $uri -Method POST -Body $data
 echo $api.StatusCode
 echo $api.Content
